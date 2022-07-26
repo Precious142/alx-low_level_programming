d697d67321c6a5cb5db737719623b20ab4b9613f
Precious142 <sunnyprecious2@gmail.com>
2022-07-26 04:34:43 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -32,7 +32,7 @@ return (len);
 int count_words(char *str)
 {
 int index = 0, words = 0, len = 0;
-for (index = 0; *(str + index0; index++)
+for (index = 0; *(str + index); index++)
 len++;
 
 for (index = 0; index < len; index++)
