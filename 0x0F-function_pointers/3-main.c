7b03febbb575b09b34d66ff51b914c3123cd4d74
Precious142 <sunnyprecious2@gmail.com>
2022-08-03 15:23:13 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
 /**
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to tht arguments.
+ * @argv: An array of pointers to the arguments.
  * Return: Always 0.
  */
 
@@ -28,7 +28,7 @@ num2 = atoi(argv[3]);
 if (get_op_func(op) == NULL || op[1] != '\0')
 {
 printf("Error\n");
-exit(98);
+exit(99);
 
 }
 if ((*op == '/' && num2 == 0) ||
